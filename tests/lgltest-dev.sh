9071e6176dd5eefb1fbf0e4b2b47ea61748ac8bc
Wong Meng Weng <mengwong@legalese.com>
2019-09-03 21:13:45 +0800

    working version of test suite; next, test against reference

--- tests/lgltest-dev.sh
+++ tests/lgltest-dev.sh
@@ -5,21 +5,33 @@ testdir=testruns/`date +%Y%m%d-%H%M%S`
 echo "*** saving to $testdir"
 mkdir -p $testdir
 cd $testdir
-export LGL_URI
-function test_services { (
-  mkdir $subdir; cd $subdir
-  echo well, here we are working against $LGL_URI
-  lgl $1 help > help.out 2> help.err
-  lgl $1 init -t > init--t.out 2> init--t.err
-  lgl $1 proforma schemalist > proforma-schemalist.out 2> proforma-schemalist.err
-  lgl $1 proforma schemalist hw3 > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
-  lgl $1 proforma schemalist hw3 example > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
-  lgl $1 proforma schema hw3 > proforma-schema-hw3.out 2> proforma-schema-hw3.err
-  lgl $1 bizfile search prive > bizfile-search-prive.out 2> bizfile-search-prive.err
-  lgl $1 bizfile uen 111111111M > bizfile-uen-111111111M.out 2> bizfile-uen-111111111M.err
-  
-) }
 # we test the development endpoints, across multiple versions
-subdir=local-v0.9 LGL_URI=http://localhost/api/v0.9 test_services --version=v0.9
-subdir=local-v1.0 LGL_URI=http://localhost/api/v1.0 test_services --version=v0.9
-subdir=local-v1.1 LGL_URI=http://localhost/api/v1.1 test_services
+subdir=local-v0.9; mkdir -p $subdir; cd $subdir;
+LGL_URI=http://localhost/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=http://localhost/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=http://localhost/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=http://localhost/api/corpsec/v0.9 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=http://localhost/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=http://localhost/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=http://localhost/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=local-v1.0; mkdir -p $subdir; cd $subdir;
+LGL_URI=http://localhost/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=http://localhost/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=http://localhost/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=http://localhost/api/corpsec/v1.0 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=http://localhost/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=http://localhost/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=http://localhost/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=local-v1.1; mkdir -p $subdir; cd $subdir;
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  bizfile search prive  > bizfile-search-prive.out 2> bizfile-search-prive.err
+LGL_URI=http://localhost/api/corpsec/v1.1 lgl  bizfile uen 111111111M  > bizfile-uen-111111111M.out 2> bizfile-uen-111111111M.err
+cd ..
--- tests/lgltest-prod.sh
+++ tests/lgltest-prod.sh
@@ -5,30 +5,120 @@ testdir=testruns/`date +%Y%m%d-%H%M%S`
 echo "*** saving to $testdir"
 mkdir -p $testdir
 cd $testdir
-export LGL_URI
-function test_services { (
-  mkdir $subdir; cd $subdir
-  echo well, here we are working against $LGL_URI
-  lgl $1 help > help.out 2> help.err
-  lgl $1 init -t > init--t.out 2> init--t.err
-  lgl $1 proforma schemalist > proforma-schemalist.out 2> proforma-schemalist.err
-  lgl $1 proforma schemalist hw3 > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
-  lgl $1 proforma schemalist hw3 example > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
-  lgl $1 proforma schema hw3 > proforma-schema-hw3.out 2> proforma-schema-hw3.err
-  lgl $1 bizfile search prive > bizfile-search-prive.out 2> bizfile-search-prive.err
-  lgl $1 bizfile uen 111111111M > bizfile-uen-111111111M.out 2> bizfile-uen-111111111M.err
-  
-) }
 # we test the production endpoints, across multiple versions
-subdir=v2-v0.9 LGL_URI=https://legalese.com/api/corpsecv0.9 test_services --version=v0.9
-subdir=v2-v1.0 LGL_URI=https://legalese.com/api/corpsecv1.0 test_services --version=v0.9
-subdir=v2-v1.1 LGL_URI=https://legalese.com/api/corpsecv1.1 test_services 
-subdir=1a-v0.9 LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsecv0.9 test_services --version=v0.9
-subdir=1a-v1.0 LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsecv1.0 test_services --version=v0.9
-subdir=1a-v1.1 LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsecv1.1 test_services 
-subdir=1b-v0.9 LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsecv0.9 test_services --version=v0.9
-subdir=1b-v1.0 LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsecv1.0 test_services --version=v0.9
-subdir=1b-v1.1 LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsecv1.1 test_services 
-subdir=api-v0.9 LGL_URI=https://api.legalese.com/api/corpsecv0.9 test_services --version=v0.9
-subdir=api-v1.0 LGL_URI=https://api.legalese.com/api/corpsecv1.0 test_services --version=v0.9
-subdir=api-v1.1 LGL_URI=https://api.legalese.com/api/corpsecv1.1 test_services
+subdir=v2-v0.9; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=v2-v1.0; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=v2-v1.1; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  bizfile search prive  > bizfile-search-prive.out 2> bizfile-search-prive.err
+LGL_URI=https://legalese.com/api/corpsec/v1.1 lgl  bizfile uen 111111111M  > bizfile-uen-111111111M.out 2> bizfile-uen-111111111M.err
+cd ..
+subdir=1a-v0.9; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=1a-v1.0; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=1a-v1.1; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  bizfile search prive  > bizfile-search-prive.out 2> bizfile-search-prive.err
+LGL_URI=https://ap-southeast-1a-api.legalese.com/api/corpsec/v1.1 lgl  bizfile uen 111111111M  > bizfile-uen-111111111M.out 2> bizfile-uen-111111111M.err
+cd ..
+subdir=1b-v0.9; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=1b-v1.0; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=1b-v1.1; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  bizfile search prive  > bizfile-search-prive.out 2> bizfile-search-prive.err
+LGL_URI=https://ap-southeast-1b-api.legalese.com/api/corpsec/v1.1 lgl  bizfile uen 111111111M  > bizfile-uen-111111111M.out 2> bizfile-uen-111111111M.err
+cd ..
+subdir=api-v0.9; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://api.legalese.com/api/corpsec/v0.9 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=api-v1.0; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.0 lgl --version=0.9 proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+cd ..
+subdir=api-v1.1; mkdir -p $subdir; cd $subdir;
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist  > proforma-schemalist.out 2> proforma-schemalist.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist hw3  > proforma-schemalist-hw3.out 2> proforma-schemalist-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  proforma schemalist hw3 example  > proforma-schemalist-hw3-example.out 2> proforma-schemalist-hw3-example.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  proforma schema hw3  > proforma-schema-hw3.out 2> proforma-schema-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 < proforma-schemalist-hw3-example.out > proforma-generate-hw3.out 2> proforma-generate-hw3.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.pdf < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.pdf.out 2> proforma-generate-hw3-hw3.pdf.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  proforma generate hw3 hw3.docx < proforma-schemalist-hw3-example.out > proforma-generate-hw3-hw3.docx.out 2> proforma-generate-hw3-hw3.docx.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  bizfile search prive  > bizfile-search-prive.out 2> bizfile-search-prive.err
+LGL_URI=https://api.legalese.com/api/corpsec/v1.1 lgl  bizfile uen 111111111M  > bizfile-uen-111111111M.out 2> bizfile-uen-111111111M.err
+cd ..
